794f5e44b4f54bc4a2682cdab3d353ae9920220b
vivesing <vivek.singh1693@gmail.com>
2022-10-06 23:52:00 +0530

    [cloud-streakm-001] adding base project for cloud stream

---
+++ src/main/java/com/viv/stream/processing/SpringCloudStreamDemoApplication.java
@@ -0,0 +1,13 @@
+package com.viv.stream.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamDemoApplication.class, args);
+	}
+
+}
